ed01645adb069bebaab6341ad671e01ed76e5d22
mayazgan <86611803+mayazgan@users.noreply.github.com>
2023-12-20 13:47:36 +0300

    Add files via upload

---
+++ Kura Çek V1.2/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace KuraCekMAYAZGAN
+{
+    partial class kuracek
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(kuracek));
+            pictureBox1 = new PictureBox();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            checkBox1 = new CheckBox();
+            label2 = new Label();
+            label3 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            coklukura = new Label();
+            pictureBox2 = new PictureBox();
+            menuStrip1 = new MenuStrip();
+            dilToolStripMenuItem = new ToolStripMenuItem();
+            türkçeToolStripMenuItem = new ToolStripMenuItem();
+            ingilizceToolStripMenuItem = new ToolStripMenuItem();
+            coklukuratxt = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(pictureBox1, "pictureBox1");
+            pictureBox1.Cursor = Cursors.Hand;
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // listBox1
+            // 
+            resources.ApplyResources(listBox1, "listBox1");
+            listBox1.FormattingEnabled = true;
+            listBox1.Name = "listBox1";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(button1, "button1");
+            button1.Name = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(textBox1, "textBox1");
+            textBox1.CharacterCasing = CharacterCasing.Upper;
+            textBox1.Name = "textBox1";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(label1, "label1");
+            label1.Name = "label1";
+            // 
+            // button2
+            // 
+            resources.ApplyResources(button2, "button2");
+            button2.Name = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            resources.ApplyResources(button3, "button3");
+            button3.Name = "button3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            resources.ApplyResources(button4, "button4");
+            button4.Name = "button4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(checkBox1, "checkBox1");
+            checkBox1.Name = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // label2
+            // 
+            resources.ApplyResources(label2, "label2");
+            label2.ForeColor = Color.IndianRed;
+            label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(label3, "label3");
+            label3.Name = "label3";
+            // 
+            // numericUpDown1
+            // 
+            resources.ApplyResources(numericUpDown1, "numericUpDown1");
+            numericUpDown1.Name = "numericUpDown1";
+            // 
+            // coklukura
+            // 
+            resources.ApplyResources(coklukura, "coklukura");
+            coklukura.Name = "coklukura";
+            // 
+            // pictureBox2
+            // 
+            resources.ApplyResources(pictureBox2, "pictureBox2");
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            resources.ApplyResources(menuStrip1, "menuStrip1");
+            menuStrip1.Items.AddRange(new ToolStripItem[] { dilToolStripMenuItem });
+            menuStrip1.Name = "menuStrip1";
+            // 
+            // dilToolStripMenuItem
+            // 
+            resources.ApplyResources(dilToolStripMenuItem, "dilToolStripMenuItem");
+            dilToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { türkçeToolStripMenuItem, ingilizceToolStripMenuItem });
+            dilToolStripMenuItem.Name = "dilToolStripMenuItem";
+            // 
+            // türkçeToolStripMenuItem
+            // 
+            resources.ApplyResources(türkçeToolStripMenuItem, "türkçeToolStripMenuItem");
+            türkçeToolStripMenuItem.Name = "türkçeToolStripMenuItem";
+            türkçeToolStripMenuItem.Click += türkçeToolStripMenuItem_Click;
+            // 
+            // ingilizceToolStripMenuItem
+            // 
+            resources.ApplyResources(ingilizceToolStripMenuItem, "ingilizceToolStripMenuItem");
+            ingilizceToolStripMenuItem.Name = "ingilizceToolStripMenuItem";
+            ingilizceToolStripMenuItem.Click += ingilizceToolStripMenuItem_Click;
+            // 
+            // coklukuratxt
+            // 
+            resources.ApplyResources(coklukuratxt, "coklukuratxt");
+            coklukuratxt.Name = "coklukuratxt";
+            // 
+            // kuracek
+            // 
+            AcceptButton = button2;
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            ControlBox = false;
+            Controls.Add(coklukuratxt);
+            Controls.Add(pictureBox2);
+            Controls.Add(coklukura);
+            Controls.Add(numericUpDown1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(checkBox1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(pictureBox1);
+            Controls.Add(menuStrip1);
+            FormBorderStyle = FormBorderStyle.None;
+            MainMenuStrip = menuStrip1;
+            Name = "kuracek";
+            Opacity = 0.95D;
+            TopMost = true;
+            FormClosing += kuracek_FormClosing;
+            Load += kuracek_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private ListBox listBox1;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private CheckBox checkBox1;
+        private Label label2;
+        private Label label3;
+        private NumericUpDown numericUpDown1;
+        private Label coklukura;
+        private PictureBox pictureBox2;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem dilToolStripMenuItem;
+        private ToolStripMenuItem türkçeToolStripMenuItem;
+        private ToolStripMenuItem ingilizceToolStripMenuItem;
+        private TextBox coklukuratxt;
+    }
+}
---
+++ Kura Çek V1.2/Form1.cs
@@ -0,0 +1,286 @@
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace KuraCekMAYAZGAN
+{
+    public partial class kuracek : Form
+    {
+        public kuracek()
+        {
+            InitializeComponent();
+        }
+
+
+
+        string hata = "";
+        string secilen = "";
+        string secilenler = "";
+        string isimboshata = "";
+        string kisisayisi0 = "";
+        string kisisayisibuyuk = "";
+        string kuralistesibos = "";
+        string kuralistesiboshata = "";
+        string seciliogeyok = "";
+
+
+
+
+
+        DialogResult dialog;
+        string cikisdialog = "";
+        string cikis = "";
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            dialog = MessageBox.Show(cikisdialog, cikis, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (dialog == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else { }
+        }
+
+        private void kuracek_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (dialog == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void kuracek_Load(object sender, EventArgs e)
+        {
+            türkçeToolStripMenuItem_Click(sender, e);            
+            textBox1.Select();                       
+            if (checkBox1.Checked == true)
+            {
+                label3.Visible = true;
+                numericUpDown1.Visible = true;
+            }
+            else
+            {
+                label3.Visible = false;
+                numericUpDown1.Visible = false;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                label3.Visible = true;
+                numericUpDown1.Visible = true;
+            }
+            else
+            {
+                label3.Visible = false;
+                numericUpDown1.Visible = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show(isimboshata, hata, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox1.Focus();
+            }
+            else
+            {
+                listBox1.Items.Add(textBox1.Text.ToString());
+                textBox1.Clear();
+                textBox1.Focus();
+            }
+        }
+
+        private void Karistir<T>(T[] array)
+        {
+            Random rastgele = new Random();
+
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                int r = rastgele.Next(0, i + 1);
+                T temp = array[i];
+                array[i] = array[r];
+                array[r] = temp;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                string kackisi = numericUpDown1.Text;                
+                int[] kisiler = new int[listBox1.Items.Count];
+                Random rnd = new Random();
+                int i = 0;
+                if (Convert.ToInt16(kackisi) == 0)
+                {
+                    MessageBox.Show(kisisayisi0, hata, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (Convert.ToInt16(numericUpDown1.Text) > listBox1.Items.Count)
+                {
+                    MessageBox.Show(kisisayisibuyuk, hata, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (Convert.ToInt16(numericUpDown1.Text) == listBox1.Items.Count)
+                {
+                    listBox1.SelectionMode = SelectionMode.MultiExtended;
+                    
+                    for (int x = 0; x < listBox1.Items.Count; x++)
+                    {
+                        listBox1.SetSelected(x, true);
+                    }
+
+                    foreach (var seciliEleman in listBox1.SelectedItems)
+                    {
+                        coklukuratxt.AppendText(seciliEleman.ToString() + Environment.NewLine);
+                    }                    
+
+                    string girilenVeriler = coklukuratxt.Text;
+                    string[] veriArray = girilenVeriler.Split('\n');
+                    Karistir(veriArray);
+                    
+                    MessageBox.Show(string.Join(coklukuratxt.Text = "" + "", veriArray), secilenler, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    
+                    coklukuratxt.Text = "";
+
+                    listBox1.SelectedItems.Clear();
+                    listBox1.SelectionMode = SelectionMode.One;
+                }
+                else
+                {
+                    if (listBox1.Items.Count > 0)
+                    {
+                        while (i < Convert.ToInt16(kackisi))
+                        {
+                            int secim = rnd.Next(0, listBox1.Items.Count);
+                            if (Array.IndexOf(kisiler, secim) == -1)
+                            {
+                                kisiler[i] = secim;
+                                coklukura.Text += listBox1.Items[secim] + "\n";
+                                i++;
+                            }
+                        }
+                        MessageBox.Show(coklukura.Text, secilenler, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        coklukura.Text = "";
+                    }
+                    else
+                    {
+                        MessageBox.Show(kuralistesiboshata, hata, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                if (listBox1.Items.Count > 0)
+                {
+                    int adet = listBox1.Items.Count;
+                    Random rnd = new Random();
+                    int kura = rnd.Next(0, adet);
+                    listBox1.SelectedIndex = kura;
+                    MessageBox.Show(listBox1.SelectedItem.ToString(), secilen, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    listBox1.SelectedItems.Clear();
+                }
+                else
+                {
+                    MessageBox.Show(kuralistesiboshata, hata, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count > 0)
+            {
+                listBox1.Items.Clear();
+            }
+            else
+            {
+                MessageBox.Show(kuralistesibos, hata, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int secili = listBox1.SelectedItems.Count;
+            if (secili == 0)
+            {
+                MessageBox.Show(seciliogeyok, hata, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                listBox1.Items.Remove(listBox1.SelectedItem);
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void türkçeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dilToolStripMenuItem.Text = "Dil";
+            türkçeToolStripMenuItem.Text = "Türkçe";
+            ingilizceToolStripMenuItem.Text = "English";
+
+            button1.Text = "Kura Çek";
+            button2.Text = "Kuraya Ekle";
+            button3.Text = "Kura Listesini Temizle";
+            button4.Text = "Seçili Olan Kiþiyi Sil";
+
+            checkBox1.Text = "Birden Fazla Kiþi Çek";
+
+            label1.Text = "Kuraya Eklenilecek Ýsim";
+            label2.Text = "KURA ÇEK V1.2";
+            label3.Text = "Kiþi Sayýsý";
+
+            cikisdialog = "Kapatmak istediðinize emin misiniz?";
+            cikis = "Çýkýþ";
+
+            hata = "Hata";
+            secilen = "Seçilen Kiþi";
+            secilenler = "Seçilen Kiþiler";
+            isimboshata = "Ýsim Boþ Olarak Kuraya Ekleme Yapýlamaz!";
+            kisisayisi0 = "Kiþi Sayýsý Sýfýr(0) Olarak Kura Çekilemez!";
+            kisisayisibuyuk = "Kiþi Sayýsý Kuradakilerin Sayýsýndan Büyük Olamaz!";
+            kuralistesibos = "Kura Listesi Zaten Boþ!";
+            kuralistesiboshata = "Kura Listesi Boþ Olarak Kura Çekimi Yapýlamaz!";
+            seciliogeyok = "Seçili Öðe Yok!";
+        }
+
+        private void ingilizceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dilToolStripMenuItem.Text = "Language";
+            türkçeToolStripMenuItem.Text = "Türkçe";
+            ingilizceToolStripMenuItem.Text = "English";
+
+            button1.Text = "Draw Lots";
+            button2.Text = "Add to Draw List";
+            button3.Text = "Clear Draw List";
+            button4.Text = "Delete Selected";
+
+            checkBox1.Text = "Draw Multiple Lots";
+
+            label1.Text = "Name To Be Added To The Draw";
+            label2.Text = "DRAW LOTS V1.2";
+            label3.Text = "How Many?";
+
+            cikisdialog = "Are you sure you want to turn it off?";
+            cikis = "Exit";
+
+            hata = "Error";
+            secilen = "Selection";
+            secilenler = "Selections";
+            isimboshata = "It is not possible to add to the draw with a blank name!";
+            kisisayisi0 = "The number of people cannot be drawn as zero(0)!";
+            kisisayisibuyuk = "The number of people cannot be greater than the number of people in the lottery!";
+            kuralistesibos = "The draw list is already empty!";
+            kuralistesiboshata = "It is not possible to draw lots with the draw list empty!";
+            seciliogeyok = "No Items Selected!";
+        }
+    }
+}
